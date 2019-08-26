8db03ddef367ea705189e54f5db4f07623e9f3b8
taoyimin <1103995288@qq.com>
2019-08-26 09:58:02 +0800

    add monitor model

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,8 +1,21 @@
-class Water {
-  Water(this.count, this.achievementRate, this.monthOnMonth, this.yearOnYear);
+import 'dart:ui';
 
+class Water {
+  Water(this.title, this.imagePath, this.count, this.achievementRate, this.monthOnMonth, this.yearOnYear);
+
+  String title; //标题
+  String imagePath; //图片路径
   int count; //数量
   double achievementRate; //达标率
   double monthOnMonth; //环比
   double yearOnYear; //同比
+}
+
+class Monitor {
+  Monitor(this.title, this.count, this.imagePath, this.color);
+
+  String title; //状态
+  int count;  //个数
+  Color color;  //图标颜色
+  String imagePath; //图片路径
 }
