f7d31ea175845b557d0f52cb29694053295a5e36
kkwangsir <417799218@qq.com>
2019-09-05 17:57:26 +0800

    add enter

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,7 +1,10 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class Water {
-  Water(this.title, this.imagePath, this.count, this.achievementRate, this.monthOnMonth, this.yearOnYear);
+  Water(this.title, this.imagePath, this.count, this.achievementRate,
+      this.monthOnMonth, this.yearOnYear);
 
   String title; //标题
   String imagePath; //图片路径
@@ -15,13 +18,14 @@ class Monitor {
   Monitor(this.title, this.count, this.imagePath, this.color);
 
   String title; //状态
-  int count;  //个数
-  Color color;  //图标颜色
+  int count; //个数
+  Color color; //图标颜色
   String imagePath; //图片路径
 }
 
 class AqiExamine {
-  AqiExamine(this.title, this.imagePath, this.title1, this.value1, this.title2, this.value2, this.title3, this.value3);
+  AqiExamine(this.title, this.imagePath, this.title1, this.value1, this.title2,
+      this.value2, this.title3, this.value3);
 
   String title; //标题
   String imagePath; //图片路径
@@ -37,17 +41,34 @@ class PollutionEnter {
   PollutionEnter(this.title, this.count, this.imagePath, this.color);
 
   String title; //标题
-  int count;  //个数
-  Color color;  //图标颜色
+  int count; //个数
+  Color color; //图标颜色
   String imagePath; //图片路径
 }
 
 class SummaryStatistics {
-  SummaryStatistics(this.title, this.count, this.color, this.imagePath, this.backgroundPath);
+  SummaryStatistics(
+      this.title, this.count, this.color, this.imagePath, this.backgroundPath);
 
   String title; //标题
-  int count;  //个数
-  Color color;  //图标颜色
+  int count; //个数
+  Color color; //图标颜色
   String imagePath; //图片路径
-  String backgroundPath;  //背景路径
+  String backgroundPath; //背景路径
+}
+
+class Enter {
+  Enter({
+    @required this.name,
+    @required this.address,
+    @required this.isImportant,
+    @required this.imagePath,
+    @required this.industryType,
+  });
+
+  String name;
+  String address;
+  bool isImportant;
+  String imagePath;
+  String industryType;
 }
