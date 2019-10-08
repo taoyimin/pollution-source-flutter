155c3fa8f75e6e1571fc6bb985e4b4d348919b65
taoyimin <1103995288@qq.com>
2019-10-08 16:28:12 +0800

    add http api class

---
+++ lib/http/http_api.dart
@@ -0,0 +1,7 @@
+class HttpApi{
+  //首页接口
+  static const String index = 'appIndex/getIndexData';
+  //登录接口
+  static const String login = 'user/login';
+  static const String orderList = 'Supervise/getReadyRemindDataByStatus?userId=666&&hasEnterprise=false&&gobalLevel=industrialPark&userLevel=4&status=1&remindStatus=2';
+}
