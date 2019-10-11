48ce5ac85f6bc6f6561619c8bff14fead6e9d5b0
taoyimin <1103995288@qq.com>
2019-10-11 11:27:59 +0800

    add monitor list api

--- lib/http/http_api.dart
+++ lib/http/http_api.dart
@@ -3,6 +3,10 @@ class HttpApi{
   static const String index = 'appIndex/getIndexData';
   //登录接口
   static const String login = 'user/login';
-  //获取督办单列表接口
+  //督办单列表
   static const String orderList = 'Supervise/getReadyRemindDataByStatus';
+  //企业列表
+  static const String enterList = 'Supervise/getReadyRemindDataByStatus?status=1';
+  //监控点列表
+  static const String monitorList = 'Supervise/getReadyRemindDataByStatus?status=1';
 }
