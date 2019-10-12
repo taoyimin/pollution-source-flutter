138ddacdb662c6a6dc41aedb1c18c87f895fea01
taoyimin <1103995288@qq.com>
2019-10-12 09:15:34 +0800

    add Statistics model

--- lib/module/common/common_model.dart
+++ lib/module/common/common_model.dart
@@ -18,3 +18,23 @@ class Label extends Equatable {
     imagePath,
   ]);
 }
+
+//统计
+class Statistics extends Equatable {
+  final String title; //标题
+  final String count; //个数
+  final Color color; //图标颜色
+  final String imagePath; //图标路径
+
+  Statistics({
+    this.title,
+    this.count,
+    this.color,
+    this.imagePath,
+  }) : super([
+    title,
+    count,
+    color,
+    imagePath,
+  ]);
+}
