95faa6da3466ba5a72f723ad7b5bc683c0a09751
taoyimin <1103995288@qq.com>
2019-10-17 10:51:32 +0800

    add PointData and ChartData model

--- lib/module/common/common_model.dart
+++ lib/module/common/common_model.dart
@@ -15,10 +15,10 @@ class Label extends Equatable {
     this.name,
     this.imagePath,
   }) : super([
-    color,
-    name,
-    imagePath,
-  ]);
+          color,
+          name,
+          imagePath,
+        ]);
 }
 
 //元数据
@@ -30,36 +30,41 @@ class Meta extends Equatable {
   final String backgroundPath; //背景图片路径
 
   Meta({
-    this.title ='标题',
-    this.content= '内容',
+    this.title = '标题',
+    this.content = '内容',
     this.color = Colours.primary_color,
     this.imagePath = '',
     this.backgroundPath = '',
   }) : super([
-    title,
-    content,
-    color,
-    imagePath,
-    backgroundPath,
-  ]);
+          title,
+          content,
+          color,
+          imagePath,
+          backgroundPath,
+        ]);
 }
 
 //附件类
-class Attachment {
+class Attachment extends Equatable {
+  final String fileName;
+  final String url;
+  final int type;
+  final String size;
+
   Attachment({
     @required this.type,
     @required this.fileName,
     @required this.url,
     @required this.size,
-  });
+  }) : super([
+          type,
+          fileName,
+          url,
+          size,
+        ]);
 
-  String fileName;
-  String url;
-  int type;
-  String size;
-
-  String get imagePath{
-    switch (type){
+  String get imagePath {
+    switch (type) {
       case 0:
         return "assets/images/icon_attachment_image.png";
       case 1:
@@ -75,18 +80,68 @@ class Attachment {
 }
 
 //处理流程
-class DealStep {
+class DealStep extends Equatable {
+  final String dealType;
+  final String dealPerson;
+  final String dealTime;
+  final String dealRemark;
+  final List<Attachment> attachmentList;
+
   DealStep({
     @required this.dealType,
     @required this.dealPerson,
     @required this.dealTime,
     @required this.dealRemark,
     @required this.attachmentList,
-  });
+  }) : super([
+          dealType,
+          dealPerson,
+          dealTime,
+          dealRemark,
+          attachmentList,
+        ]);
+}
 
-  String dealType;
-  String dealPerson;
-  String dealTime;
-  String dealRemark;
-  List<Attachment> attachmentList;
+class PointData extends Equatable {
+  final double x;
+  final double y;
+
+  PointData({
+    @required this.x,
+    @required this.y,
+  }) : super([
+          x,
+          y,
+        ]);
+}
+
+class ChartData extends Equatable {
+  final String factorName;
+  final bool show;
+  final double maxX;
+  final double minX;
+  final double maxY;
+  final double minY;
+  final Color color;
+  final List<PointData> points;
+
+  ChartData({
+    @required this.factorName,
+    @required this.show,
+    @required this.color,
+    @required this.maxX,
+    @required this.maxY,
+    @required this.minX,
+    @required this.minY,
+    @required this.points,
+  }) : super([
+          factorName,
+          show,
+          color,
+          maxX,
+          maxY,
+          minX,
+          minY,
+          points,
+        ]);
 }
