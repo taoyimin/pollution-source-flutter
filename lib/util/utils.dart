f42f8b6974128a27dc43eaf88a41b3c77faf1706
taoyimin <1103995288@qq.com>
2019-10-21 18:18:06 +0800

    add common utils

---
+++ lib/util/utils.dart
@@ -0,0 +1,186 @@
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pollution_source/module/common/common_model.dart';
+import 'package:pollution_source/util/toast_utils.dart';
+import 'package:keyboard_actions/keyboard_actions.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Utils {
+  /// 调起拨号页
+  static void launchTelURL(String phone) async {
+    String url = 'tel:' + phone;
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      Toast.show('拨号失败！');
+    }
+  }
+
+  /// 调起二维码扫描页
+  static Future<String> scan() async {
+    try {
+      return await BarcodeScanner.scan();
+    } catch (e) {
+      if (e is PlatformException) {
+        if (e.code == BarcodeScanner.CameraAccessDenied) {
+          Toast.show("没有相机权限！");
+        }
+      }
+    }
+    return null;
+  }
+
+  static KeyboardActionsConfig getKeyboardActionsConfig(List<FocusNode> list) {
+    return KeyboardActionsConfig(
+      keyboardActionsPlatform: KeyboardActionsPlatform.IOS,
+      keyboardBarColor: Colors.grey[200],
+      nextFocus: true,
+      actions: List.generate(
+          list.length,
+          (i) => KeyboardAction(
+                focusNode: list[i],
+                closeWidget: const Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: const Text("关闭"),
+                ),
+              )),
+    );
+  }
+
+  //获取一个数组中的最大数
+  static double getMax(List<double> items) {
+    if (items.length == 0) return 0;
+    double temp = items[0];
+    items.forEach((item) {
+      if (item > temp) {
+        temp = item;
+      }
+    });
+    return temp;
+  }
+
+  //获取一个数组中的最小数
+  static double getMin(List<double> items) {
+    if (items.length == 0) return 0;
+    double temp = items[0];
+    items.forEach((item) {
+      if (item < temp) {
+        temp = item;
+      }
+    });
+    return temp;
+  }
+
+  static double getYAxisInterval(List<ChartData> chartDataList) {
+    double maxY = Utils.getMax(chartDataList.where((chartData)=>chartData.checked).map((chartData) {
+      return chartData.maxY;
+    }).toList());
+    double minY = Utils.getMin(chartDataList.where((chartData)=>chartData.checked).map((chartData) {
+      return chartData.minY;
+    }).toList());
+    return (maxY - minY) / (4);
+  }
+
+  static double getXAxisInterval(List<ChartData> chartDataList) {
+    double maxX = Utils.getMax(chartDataList.where((chartData)=>chartData.checked).map((chartData) {
+      return chartData.maxX;
+    }).toList());
+    double minX = Utils.getMin(chartDataList.where((chartData)=>chartData.checked).map((chartData) {
+      return chartData.minX;
+    })?.toList());
+    return (maxX - minX) / (6);
+  }
+}
+
+/// 默认dialog背景色为半透明黑色，这里修改源码改为透明
+Future<T> showTransparentDialog<T>({
+  @required BuildContext context,
+  bool barrierDismissible = true,
+  WidgetBuilder builder,
+}) {
+  final ThemeData theme = Theme.of(context, shadowThemeOnly: true);
+  return showGeneralDialog(
+    context: context,
+    pageBuilder: (BuildContext buildContext, Animation<double> animation,
+        Animation<double> secondaryAnimation) {
+      final Widget pageChild = Builder(builder: builder);
+      return SafeArea(
+        child: Builder(builder: (BuildContext context) {
+          return theme != null
+              ? Theme(data: theme, child: pageChild)
+              : pageChild;
+        }),
+      );
+    },
+    barrierDismissible: barrierDismissible,
+    barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
+    barrierColor: const Color(0x00FFFFFF),
+    transitionDuration: const Duration(milliseconds: 150),
+    transitionBuilder: _buildMaterialDialogTransitions,
+  );
+}
+
+Widget _buildMaterialDialogTransitions(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child) {
+  return FadeTransition(
+    opacity: CurvedAnimation(
+      parent: animation,
+      curve: Curves.easeOut,
+    ),
+    child: child,
+  );
+}
+
+Future<T> showElasticDialog<T>({
+  @required BuildContext context,
+  bool barrierDismissible = true,
+  WidgetBuilder builder,
+}) {
+  final ThemeData theme = Theme.of(context, shadowThemeOnly: true);
+  return showGeneralDialog(
+    context: context,
+    pageBuilder: (BuildContext buildContext, Animation<double> animation,
+        Animation<double> secondaryAnimation) {
+      final Widget pageChild = Builder(builder: builder);
+      return SafeArea(
+        child: Builder(builder: (BuildContext context) {
+          return theme != null
+              ? Theme(data: theme, child: pageChild)
+              : pageChild;
+        }),
+      );
+    },
+    barrierDismissible: barrierDismissible,
+    barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
+    barrierColor: Colors.black54,
+    transitionDuration: const Duration(milliseconds: 550),
+    transitionBuilder: _buildDialogTransitions,
+  );
+}
+
+Widget _buildDialogTransitions(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child) {
+  return FadeTransition(
+    opacity: CurvedAnimation(
+      parent: animation,
+      curve: Curves.easeOut,
+    ),
+    child: SlideTransition(
+      position: Tween<Offset>(begin: const Offset(0.0, 0.3), end: Offset.zero)
+          .animate(CurvedAnimation(
+        parent: animation,
+        curve: animation.status != AnimationStatus.forward
+            ? Curves.easeOutBack
+            : ElasticOutCurve(0.85),
+      )),
+      child: child,
+    ),
+  );
+}
