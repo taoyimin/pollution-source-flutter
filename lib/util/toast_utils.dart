354130a9e28d87c57595403f4c4f6f64f629d58e
taoyimin <1103995288@qq.com>
2019-10-21 18:17:38 +0800

    add toast utils

---
+++ lib/util/toast_utils.dart
@@ -0,0 +1,16 @@
+import 'package:oktoast/oktoast.dart';
+
+/// Toast工具类
+class Toast {
+  static show(String msg, {duration = 2000}) {
+    showToast(
+        msg,
+        duration: Duration(milliseconds: duration),
+        dismissOtherToast: true
+    );
+  }
+
+  static cancelToast() {
+    dismissAllToast();
+  }
+}
