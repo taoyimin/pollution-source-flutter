a975ae2f0d8f913aa8579f64b4515afdaa46a110
taoyimin <1103995288@qq.com>
2019-11-13 17:40:05 +0800

    regular update

--- lib/http/http_api.dart
+++ lib/http/http_api.dart
@@ -5,20 +5,24 @@ class HttpApi{
   static const String login = 'user/login';
   //报警管理单列表
   static const String orderList = 'Supervise/getReadyRemindDataByStatus';
-  //报警管理单详情 TODO
-  static const String orderDetail = 'enterprise/queryALLEnter';
+  //报警管理单详情
+  static const String orderDetail = '报警管理单详情接口';
   //异常申报单列表
   static const String reportList = 'stopApply/getApplyList';
   //异常申报单详情
   static const String reportDetail = 'stopApply/getStopApply';
   //企业列表
   static const String enterList = 'enterprise/queryALLEnter';
-  //企业详情 TODO
-  static const String enterDetail = 'enterprise/queryALLEnter';
+  //企业详情
+  static const String enterDetail = 'enterprise/queryEnterByEntId';
+  //排口列表
+  static const String dischargeList = '排口列表接口';
   //监控点列表
   static const String monitorList = 'tDisChargeOut/getDrainInfo';
-  //监控点详情 TODO
-  static const String monitorDetail = 'enterprise/queryALLEnter';
+  //排口详情
+  static const String dischargeDetail = '排口详情接口';
+  //监控点详情
+  static const String monitorDetail = '监控点详情接口';
   //排污许可证详情
   static const String licenseDetail = 'enterprise/getLicenseInfoById';
 }
