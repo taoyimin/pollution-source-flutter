fc1810e5f7cfda0f2bac5909ce611ff97c17ce83
taoyimin <1103995288@qq.com>
2019-11-20 15:49:35 +0800

    add python api

--- lib/http/http_api.dart
+++ lib/http/http_api.dart
@@ -1,4 +1,4 @@
-class HttpApi{
+class HttpApiJava{
   //首页接口
   static const String index = 'appIndex/getIndexData';
   //登录接口
@@ -6,7 +6,7 @@ class HttpApi{
   //报警管理单列表
   static const String orderList = 'Supervise/getReadyRemindDataByStatus';
   //报警管理单详情
-  static const String orderDetail = '报警管理单详情接口';
+  static const String orderDetail = 'Supervise/queryAllFlowById';
   //异常申报单列表
   static const String reportList = 'stopApply/getApplyList';
   //异常申报单详情
@@ -16,13 +16,40 @@ class HttpApi{
   //企业详情
   static const String enterDetail = 'enterprise/queryEnterByEntId';
   //排口列表
-  static const String dischargeList = '排口列表接口';
+  static const String dischargeList = 'tDisChargeOut/getDisChageOut';
   //监控点列表
   static const String monitorList = 'tDisChargeOut/getDrainInfo';
   //排口详情
-  static const String dischargeDetail = '排口详情接口';
+  static const String dischargeDetail = 'tDisChargeOut/getDisChageOut';
   //监控点详情
-  static const String monitorDetail = '监控点详情接口';
+  static const String monitorDetail = 'tDisChargeOut/getDrainInfoById';
+  //排污许可证列表
+  static const String licenseList = '排污许可证列表接口';
   //排污许可证详情
   static const String licenseDetail = 'enterprise/getLicenseInfoById';
+}
+
+class HttpApiPython{
+  //首页
+  static const String index = 'index';
+  //token
+  static const String token = 'token';
+  //企业
+  static const String enters = 'enters';
+  //排口
+  static const String discharges = 'discharges';
+  //监控点
+  static const String monitors = 'monitors';
+  //报警管理单
+  static const String orders = 'orders';
+  //异常申报单
+  static const String reports = 'reports';
+  //排口异常申报
+  static const String dischargeReports = 'dischargeReports';
+  //因子异常申报
+  static const String factorReports = 'factorReports';
+  //长期停产申报
+  static const String longStopReports = 'longStopReports';
+  //排污许可证
+  static const String licenses = 'licenses';
 }
