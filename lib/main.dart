ee4422ff1b0509c7e27cbda8c7c08e0db76a53c9
taoyimin <1103995288@qq.com>
2019-11-27 20:04:19 +0800

    regular update

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,33 @@
 import 'package:flustars/flustars.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:oktoast/oktoast.dart';
 import 'package:pollution_source/page/login.dart';
 import 'package:pollution_source/res/colors.dart';
 
 void main() async {
+  //初始化SpUtil
   await SpUtil.getInstance();
   runApp(MyApp());
   //状态栏白色字体
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
 }
 
-/// This Widget is the main application widget.
+//This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colours.primary_color,
-        accentColor: Colours.accent_color,
-        brightness: Brightness.light,
-        primaryColorBrightness: Brightness.dark,
+    return OKToast(
+      child: MaterialApp(
+        theme: ThemeData(
+          primaryColor: Colours.primary_color,
+          accentColor: Colours.accent_color,
+          brightness: Brightness.light,
+          primaryColorBrightness: Brightness.dark,
+        ),
+        home: LoginPage(),
       ),
-      title: _title,
-      home: LoginPage(),
     );
   }
 }
