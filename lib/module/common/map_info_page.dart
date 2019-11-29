589e2e527f142b10934277c314f04a0146f4b020
taoyimin <1103995288@qq.com>
2019-11-29 16:49:58 +0800

    add common map info page

---
+++ lib/module/common/map_info_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:pollution_source/res/gaps.dart';
+
+import 'common_widget.dart';
+
+//展示信息通用页面
+class MapInfoPage extends StatelessWidget {
+  final String title;
+  //要展示的信息 key：标题 value：内容
+  final Map<String, dynamic> mapInfo;
+
+  MapInfoPage({@required this.title, @required this.mapInfo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('$title'),
+      ),
+      body: _buildBodyWidget(),
+    );
+  }
+
+  Widget _buildBodyWidget() {
+    return ListView.separated(
+      itemCount: mapInfo.length,
+      itemBuilder: (BuildContext context, int index) {
+        return MapInfoRowWidget(
+          title: '${mapInfo.keys.toList()[index]}',
+          content: '${mapInfo.values.toList()[index]}',
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => Gaps.hLine,
+    );
+  }
+}
