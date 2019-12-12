b9ddefe59ea5bb36889122e1cf14fd7cb9a38692
taoyimin <1103995288@qq.com>
2019-12-12 21:01:10 +0800

    init route config

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:fluro/fluro.dart';
 import 'package:flustars/flustars.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:oktoast/oktoast.dart';
-import 'package:pollution_source/page/login.dart';
 import 'package:pollution_source/res/colors.dart';
+import 'package:pollution_source/route/application.dart';
+import 'package:pollution_source/route/routes.dart';
 
 void main() async {
   //初始化SpUtil
@@ -14,13 +16,23 @@ void main() async {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
 }
 
-//This Widget is the main application widget.
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  _MyAppState() {
+    final router = Router();
+    Routes.configureRoutes(router);
+    Application.router = router;
+  }
 
   @override
   Widget build(BuildContext context) {
     return OKToast(
       child: MaterialApp(
+        //设置中文本地化
         localizationsDelegates: [
           GlobalMaterialLocalizations.delegate,
           GlobalWidgetsLocalizations.delegate,
@@ -35,8 +47,40 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.light,
           primaryColorBrightness: Brightness.dark,
         ),
-        home: LoginPage(),
+        onGenerateRoute: Application.router.generator,
       ),
     );
   }
 }
+
+/*class MyApp extends StatelessWidget {
+  MyApp() {
+    final router = Router();
+    Routes.configureRoutes(router);
+    Application.router = router;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return OKToast(
+      child: MaterialApp(
+        //设置中文本地化
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale.fromSubtags(languageCode: 'zh'),
+        ],
+        theme: ThemeData(
+          primaryColor: Colours.primary_color,
+          accentColor: Colours.accent_color,
+          brightness: Brightness.light,
+          primaryColorBrightness: Brightness.dark,
+        ),
+        onGenerateRoute: Application.router.generator,
+      ),
+    );
+  }
+}*/
