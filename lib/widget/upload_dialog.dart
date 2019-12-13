374e6eaba4b39fe6770b7ee14a45a35ab2b5b074
taoyimin <1103995288@qq.com>
2019-12-13 17:43:48 +0800

    add cancel button

--- lib/widget/upload_dialog.dart
+++ lib/widget/upload_dialog.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pollution_source/module/common/upload/upload_bloc.dart';
+import 'package:pollution_source/module/common/upload/upload_state.dart';
+import 'package:pollution_source/res/gaps.dart';
 
 class UploadDialog extends Dialog {
   final UploadBloc uploadBloc;
@@ -14,40 +16,39 @@ class UploadDialog extends Dialog {
       },
       child: Dialog(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.4,
-          width: MediaQuery.of(context).size.width * 0.8,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    height: (MediaQuery.of(context).size.height / 2) * 0.6,
-                    child: Card(
-                      elevation: 0.0,
-                      margin: const EdgeInsets.all(0.0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(8),
-                          topLeft: Radius.circular(8),
-                        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: (MediaQuery.of(context).size.height / 2) * 0.6,
+                  child: Card(
+                    elevation: 0.0,
+                    margin: const EdgeInsets.all(0.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(8),
+                        topLeft: Radius.circular(8),
                       ),
-                      clipBehavior: Clip.antiAlias,
-                      child: Image.asset(
-                        'assets/images/men_wearing_jackets.gif',
-                        fit: BoxFit.cover,
-                      ),
+                    ),
+                    clipBehavior: Clip.antiAlias,
+                    child: Image.asset(
+                      'assets/images/men_wearing_jackets.gif',
+                      fit: BoxFit.cover,
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 20),
-                    child: const Text(
-                      '上传中，请耐心等待...',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    /*child: BlocBuilder<UploadBloc, UploadState>(
+                ),
+                Gaps.vGap10,
+                const Text(
+                  '上传中，请耐心等待...',
+                  style: TextStyle(fontSize: 16),
+                ),
+                /*Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  child: BlocBuilder<UploadBloc, UploadState>(
                       bloc: uploadBloc,
                       builder: (context, state) {
                         if (state is UploadSuccess) {
@@ -58,12 +59,36 @@ class UploadDialog extends Dialog {
                           return Text('未知状态');
                         }
                       },
-                    ),*/
+                    ),
+                ),*/
+                Gaps.vGap10,
+                SizedBox(
+                  width: 100,
+                  child: FlatButton(
+                    onPressed: () {
+                      final currentState = uploadBloc.state;
+                      if(currentState is Uploading){
+                        currentState.token.cancel('取消上传');
+                      }
+                    },
+                    padding:const EdgeInsets.symmetric(horizontal: 6, vertical: 6),
+                    color: Colors.redAccent,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        const Text(
+                          '取消',
+                          style: TextStyle(color: Colors.white, fontSize: 13),
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+                Gaps.vGap10,
+              ],
+            ),
+          ],
         ),
       ),
     );
