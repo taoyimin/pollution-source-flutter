13e128a0bdf9d678d9e28f325f417104d060f057
taoyimin <1103995288@qq.com>
2019-12-20 13:16:17 +0800

    DataDict model add checked field and copyWith method

--- lib/module/common/common_model.dart
+++ lib/module/common/common_model.dart
@@ -243,21 +243,38 @@ class ChartData extends Equatable {
 }
 
 /// 数据字典类
+///
+/// [checked]默认为false，只有多选时才会用到
 @JsonSerializable()
 class DataDict extends Equatable {
   final String code;
   final String name;
+  final bool checked;
 
   const DataDict({
     @required this.code,
     @required this.name,
+    this.checked = false,
   });
 
   @override
   List<Object> get props => [
         code,
         name,
+        checked,
       ];
+
+  DataDict copyWith({
+    String code,
+    String name,
+    bool checked,
+  }) {
+    return DataDict(
+      code: code ?? this.code,
+      name: name ?? this.name,
+      checked: checked ?? this.checked,
+    );
+  }
 
   factory DataDict.fromJson(Map<String, dynamic> json) =>
       _$DataDictFromJson(json);
