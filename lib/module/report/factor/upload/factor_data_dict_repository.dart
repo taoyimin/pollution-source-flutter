85e46c7f9d930ad1426dca7413b6f808856191f9
taoyimin <1103995288@qq.com>
2020-01-07 14:31:25 +0800

    add factor data dict repository

---
+++ lib/module/report/factor/upload/factor_data_dict_repository.dart
@@ -0,0 +1,13 @@
+import 'package:pollution_source/http/http_api.dart';
+import 'package:pollution_source/module/common/common_model.dart';
+import 'package:pollution_source/module/common/dict/data_dict_repository.dart';
+import 'package:pollution_source/module/report/factor/upload/factor_data_dict_model.dart';
+
+class FactorDataDictRepository extends DataDictRepository {
+  FactorDataDictRepository(HttpApi httpApi) : super(httpApi);
+
+  @override
+  DataDict fromJson(json) {
+    return FactorDataDict.fromJson(json);
+  }
+}
