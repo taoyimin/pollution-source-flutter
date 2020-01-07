9104164bc91b1aa53cf741b975984f3448f519ca
taoyimin <1103995288@qq.com>
2020-01-07 14:31:03 +0800

    add factor data dict

---
+++ lib/module/report/factor/upload/factor_data_dict_model.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:pollution_source/module/common/common_model.dart';
+
+part 'factor_data_dict_model.g.dart';
+
+/// 异常因子类
+@JsonSerializable()
+class FactorDataDict extends DataDict {
+  @JsonKey(name: 'Factor_Code')
+  final String code;
+  @JsonKey(name: 'Factor_Name')
+  final String name;
+
+  const FactorDataDict({this.code, this.name}) : super(code: code, name: name);
+
+  factory FactorDataDict.fromJson(Map<String, dynamic> json) =>
+      _$FactorDataDictFromJson(json);
+
+  Map<String, dynamic> toJson() => _$FactorDataDictToJson(this);
+}
