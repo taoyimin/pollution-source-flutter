d223d323ffb22e346990809f09096efdf6de6f11
taoyimin <1103995288@qq.com>
2020-03-26 17:11:06 +0800

    add share product page

---
+++ lib/page/share_product_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:pollution_source/module/common/common_widget.dart';
+import 'package:pollution_source/res/colors.dart';
+import 'package:pollution_source/util/compat_utils.dart';
+import 'package:share/share.dart';
+
+/// 修改密码界面
+class ShareProductPage extends StatefulWidget {
+  @override
+  _ShareProductPageState createState() => _ShareProductPageState();
+}
+
+class _ShareProductPageState extends State<ShareProductPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('分享产品'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 46),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 1,
+              child: Image.asset(CompatUtils.getDownloadQRcode()),
+            ),
+            Text(
+              '扫码下载或点击下方按钮进行分享',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 17,
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                ClipButton(
+                  text: '点我分享',
+                  icon: Icons.share,
+                  color: Colours.primary_color,
+                  onTap: () {
+                    Share.share(
+                      '【下载地址】${CompatUtils.getDownloadUrl()}',
+                      subject: '下载地址：',
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
