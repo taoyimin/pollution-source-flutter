c781275c0d76331e88236b9bc04a5da17c7a0eb2
taoyimin <1103995288@qq.com>
2020-04-10 11:05:22 +0800

    regular update

--- lib/module/monitor/list/monitor_list_model.dart
+++ lib/module/monitor/list/monitor_list_model.dart
@@ -1,26 +1,22 @@
-import 'package:common_utils/common_utils.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flustars/flustars.dart';
-import 'package:flutter/material.dart';
 import 'package:json_annotation/json_annotation.dart';
-import 'package:pollution_source/module/common/common_model.dart';
 
 part 'monitor_list_model.g.dart';
 
-//监控点列表
+/// 监控点列表
 @JsonSerializable()
 class Monitor extends Equatable {
   @JsonKey(name: 'outId')
-  final int dischargeId; //排口ID
-  final int monitorId; //监控点ID
+  final int dischargeId; // 排口ID
+  final int monitorId; // 监控点ID
   @JsonKey(name: 'enterpriseName')
-  final String enterName; //企业名称
+  final String enterName; // 企业名称
   @JsonKey(name: 'disMonitorName')
-  final String monitorName; //监控点名称
+  final String monitorName; // 监控点名称
   @JsonKey(name: 'disMonitorType')
-  final String monitorType; //监控点类型
+  final String monitorType; // 监控点类型
   @JsonKey(name: 'outletTypeStr')
-  final String monitorCategoryStr; //监控点类别
+  final String monitorCategoryStr; // 监控点类别
 
   const Monitor({
     this.dischargeId,
@@ -45,12 +41,6 @@ class Monitor extends Equatable {
     return _getMonitorTypeImage(monitorType);
   }
 
-  List<Label> get labelList {
-    return TextUtil.isEmpty('')
-        ? const []
-        : _getLabelList('流量* PH* 化学需氧量* 氨氮* 总磷* 总氮*');
-  }
-
   factory Monitor.fromJson(Map<String, dynamic> json) =>
       _$MonitorFromJson(json);
 
@@ -60,27 +50,17 @@ class Monitor extends Equatable {
   static String _getMonitorTypeImage(String monitorType) {
     switch (monitorType) {
       case 'outletType1':
-        //雨水
+        // 雨水
         return 'assets/images/icon_unknown_monitor.png';
       case 'outletType2':
-        //废水
+        // 废水
         return 'assets/images/icon_water_monitor.png';
       case 'outletType3':
-        //废气
+        // 废气
         return 'assets/images/icon_air_monitor.png';
       default:
-        //未知
+        // 未知
         return 'assets/images/icon_unknown_monitor.png';
     }
-  }
-
-  //将因子string转化成List
-  static List<Label> _getLabelList(String string) {
-    return string.trimLeft().trimRight().split(' ').map((string) {
-      return Label(
-        name: string,
-        color: Colors.green,
-      );
-    }).toList();
   }
 }
