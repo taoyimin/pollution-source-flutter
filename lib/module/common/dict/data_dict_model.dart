a217ae1c5049339bebe927c49ce64290649517ab
taoyimin <1103995288@qq.com>
2020-04-22 10:25:46 +0800

    add DataDict model

---
+++ lib/module/common/dict/data_dict_model.dart
@@ -0,0 +1,48 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
+
+part 'data_dict_model.g.dart';
+
+/// 数据字典类
+///
+/// [checked]默认为false，只有多选时才会用到
+@JsonSerializable()
+class DataDict extends Equatable {
+  @JsonKey(name: 'dicSubCode')
+  final String code;
+  @JsonKey(name: 'dicSubName')
+  final String name;
+  @JsonKey(ignore: true)
+  final bool checked;
+
+  const DataDict({
+    @required this.code,
+    @required this.name,
+    this.checked = false,
+  });
+
+  @override
+  List<Object> get props => [
+        code,
+        name,
+        checked,
+      ];
+
+  DataDict copyWith({
+    String code,
+    String name,
+    bool checked,
+  }) {
+    return DataDict(
+      code: code ?? this.code,
+      name: name ?? this.name,
+      checked: checked ?? this.checked,
+    );
+  }
+
+  factory DataDict.fromJson(Map<String, dynamic> json) =>
+      _$DataDictFromJson(json);
+
+  Map<String, dynamic> toJson() => _$DataDictToJson(this);
+}
