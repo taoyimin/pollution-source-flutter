418096e15de9b22a23b36651f337e8a25fb80493
taoyimin <1103995288@qq.com>
2020-04-22 11:10:41 +0800

    move factor data dict module

--- lib/module/common/dict/factor/factor_data_dict_repository.dart
+++ lib/module/common/dict/factor/factor_data_dict_repository.dart
@@ -1,7 +1,8 @@
 import 'package:pollution_source/http/http_api.dart';
 import 'package:pollution_source/module/common/dict/data_dict_model.dart';
 import 'package:pollution_source/module/common/dict/data_dict_repository.dart';
-import 'package:pollution_source/module/report/factor/upload/factor_data_dict_model.dart';
+
+import 'factor_data_dict_model.dart';
 
 class FactorDataDictRepository extends DataDictRepository {
   FactorDataDictRepository(HttpApi httpApi) : super(httpApi);
