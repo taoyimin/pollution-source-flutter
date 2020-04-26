ec81103540eae9db8691453defb33342c1a5d008
taoyimin <1103995288@qq.com>
2020-04-26 15:42:31 +0800

    delete methods

--- lib/util/ui_utils.dart
+++ lib/util/ui_utils.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
-import 'package:pollution_source/module/common/common_model.dart';
 
 /// UI工具类
 ///
@@ -68,64 +67,6 @@ class UIUtils {
     );
   }
 
-  /// 获取一个数组中的最大数
-  static double getMax(List<double> items) {
-    if (items.length == 0) return 0;
-    double temp = items[0];
-    items.forEach((item) {
-      if (item > temp) {
-        temp = item;
-      }
-    });
-    return temp;
-  }
-
-  /// 获取一个数组中的最小数
-  static double getMin(List<double> items) {
-    if (items.length == 0) return 0;
-    double temp = items[0];
-    items.forEach((item) {
-      if (item < temp) {
-        temp = item;
-      }
-    });
-    return temp;
-  }
-
-  /// 获取Y轴的间隔（坐标轴默认显示5个坐标）
-  static double getYAxisInterval(List<ChartData> chartDataList) {
-    double maxY = UIUtils.getMax(
-        chartDataList.where((chartData) => chartData.checked).map((chartData) {
-      return chartData.maxY;
-    }).toList());
-    double minY = UIUtils.getMin(
-        chartDataList.where((chartData) => chartData.checked).map((chartData) {
-      return chartData.minY;
-    }).toList());
-    return (maxY - minY) / (4);
-  }
-
-  /// 获取X轴的间隔（坐标轴默认显示7个坐标）
-  static double getXAxisInterval(List<ChartData> chartDataList) {
-    double maxX = UIUtils.getMax(
-        chartDataList.where((chartData) => chartData.checked).map((chartData) {
-      return chartData.maxX;
-    }).toList());
-    double minX = UIUtils.getMin(
-        chartDataList.where((chartData) => chartData.checked).map((chartData) {
-      return chartData.minX;
-    })?.toList());
-    return (maxX - minX) / (6);
-  }
-
-  /// 判断String是否是数字
-  static bool isNumeric(String str) {
-    if (str == null) {
-      return false;
-    }
-    return double.tryParse(str) != null;
-  }
-
   static Color getAlarmFlagColor(String alarmFlag) {
     switch (alarmFlag) {
       case '0':
@@ -154,17 +95,5 @@ class UIUtils {
       default:
         return Colors.black;
     }
-  }
-
-  /// 获取两个时间中更小的时间
-  static DateTime getMaxDateTime(DateTime dateTime1, DateTime dateTime2) {
-    if (dateTime1 == null)
-      return dateTime2;
-    else if (dateTime2 == null)
-      return dateTime1;
-    else if (dateTime1.isAfter(dateTime2))
-      return dateTime1;
-    else
-      return dateTime2;
   }
 }
