8c850bde4b2ecb261d907a489fe95a98a7f82ad7
taoyimin <1103995288@qq.com>
2020-05-06 09:04:54 +0800

    regular update

--- lib/module/process/upload/process_upload_repository.dart
+++ lib/module/process/upload/process_upload_repository.dart
@@ -11,15 +11,20 @@ import 'package:pollution_source/module/process/upload/process_upload_model.dart
 class ProcessUploadRepository extends UploadRepository<ProcessUpload, String> {
   @override
   checkData(ProcessUpload data) {
-    if (TextUtil.isEmpty(data.orderId))
+    if (data.orderId == null)
       throw DioError(error: InvalidParamException('报警管理单Id为空'));
+    if (TextUtil.isEmpty(data.alarmState))
+      throw DioError(error: InvalidParamException('报警管理单状态为空'));
     if (TextUtil.isEmpty(data.operatePerson))
       throw DioError(error: InvalidParamException('请输入操作人'));
     if (TextUtil.isEmpty(data.operateType))
       throw DioError(error: InvalidParamException('操作类型为空'));
+    if (data.alarmCauseList == null || data.alarmCauseList.length == 0)
+      throw DioError(error: InvalidParamException('请选择报警原因'));
     if (TextUtil.isEmpty(data.operateDesc))
       throw DioError(error: InvalidParamException('请输入操作描述'));
-    if (data.attachments == null || data.attachments.length == 0)
+    if (data.operateType == '-1' && (data.attachments == null || data.attachments.length == 0))
+      // 处理督办单必须上传附件
       throw DioError(error: InvalidParamException('请选择附件上传'));
   }
 
@@ -31,10 +36,11 @@ class ProcessUploadRepository extends UploadRepository<ProcessUpload, String> {
   @override
   Future<FormData> createFormData(ProcessUpload data) async {
     return FormData.fromMap({
-      'id': data.orderId,
-      'orderId': data.orderId,
+      'id': data.orderId.toString(),
+      'alarmState': data.alarmState,
       'operatePerson': data.operatePerson,
-      'operateType': data.operateType,
+      'audit': data.operateType,
+      'alarmCause': data.alarmCauseList.map((dataDict) => dataDict.code).join(','),
       'operateDesc': data.operateDesc,
       "file": await Future.wait(data.attachments?.map((asset) async {
         ByteData byteData = await asset.getByteData();
