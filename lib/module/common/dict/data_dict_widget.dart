f11eba0093b9ba4bbfd762335c4e18730f04234e
taoyimin <1103995288@qq.com>
2020-05-07 11:07:07 +0800

    DataDictDialog add scroll

--- lib/module/common/dict/data_dict_widget.dart
+++ lib/module/common/dict/data_dict_widget.dart
@@ -64,7 +64,7 @@ class DataDictWidget extends StatelessWidget {
                             onPressed: () {
                               Clipboard.setData(
                                   ClipboardData(text: '${state.message}'));
-                              Toast.show('复制成功！');
+                              Toast.show('复制到剪贴板成功！');
                               Navigator.of(context).pop();
                             },
                             child: const Text("复制"),
@@ -163,172 +163,6 @@ class DataDictWidget extends StatelessWidget {
                             .map<PopupMenuItem<DataDict>>((dataDict) {
                           return PopupMenuItem<DataDict>(
                             value: dataDict,
-                            child: Text('${dataDict.name}'),
-                          );
-                        }).toList();
-                      } else {
-                        return [];
-                      }
-                    },
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-/// 数据字典多选控件
-///
-/// [selected]参数为默认已经选中的选项，其他参数项参考[DataDictWidget]单选控件
-@Deprecated('已弃用')
-class DataDictMultiWidget extends StatelessWidget {
-  final String title;
-  final String content;
-  final String tipText;
-  final List<DataDict> selected;
-  final DataDictBloc dataDictBloc;
-  final PopupMenuItemSelected<DataDict> onSelected;
-  final double height;
-
-  DataDictMultiWidget({
-    Key key,
-    @required this.title,
-    @required this.content,
-    this.tipText = '还未完成初始化，无法选择',
-    @required this.selected,
-    @required this.dataDictBloc,
-    @required this.onSelected,
-    this.height = 46,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocListener<DataDictBloc, DataDictState>(
-      bloc: dataDictBloc,
-      listener: (context, state) {
-        if (state is DataDictError) {
-          Scaffold.of(context).showSnackBar(
-            SnackBar(
-              content: Text('$title加载失败！'),
-              action: SnackBarAction(
-                  label: '查看详情',
-                  textColor: Colours.primary_color,
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: const Text("错误信息"),
-                          content: SingleChildScrollView(
-                            child: Text('${state.message}'),
-                          ),
-                          actions: <Widget>[
-                            FlatButton(
-                              onPressed: () {
-                                Clipboard.setData(
-                                    ClipboardData(text: '${state.message}'));
-                                Toast.show('复制成功！');
-                                Navigator.of(context).pop();
-                              },
-                              child: const Text("复制"),
-                            ),
-                            FlatButton(
-                              onPressed: () async {
-                                Navigator.of(context).pop();
-                              },
-                              child: const Text("确认"),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  }),
-            ),
-          );
-        }
-      },
-      child: BlocBuilder<DataDictBloc, DataDictState>(
-        bloc: dataDictBloc,
-        builder: (context, state) {
-          return Container(
-            height: height,
-            child: Row(
-              children: <Widget>[
-                Text(
-                  '$title',
-                  style: const TextStyle(fontSize: 15),
-                ),
-                Gaps.hGap20,
-                Expanded(
-                  flex: 1,
-                  child: PopupMenuButton<DataDict>(
-                    child: InkWell(
-                      onTap: state is DataDictError || state is DataDictInitial
-                          ? () {
-                              if (state is DataDictError) {
-                                // 如果加载失败，则点击重新加载
-                                dataDictBloc.add(DataDictLoad());
-                              } else if (state is DataDictInitial) {
-                                // 如果还未初始化，则显示提示信息
-                                Scaffold.of(context).showSnackBar(
-                                  SnackBar(
-                                    content: Text('$tipText'),
-                                    action: SnackBarAction(
-                                        label: '我知道了',
-                                        textColor: Colours.primary_color,
-                                        onPressed: () {}),
-                                  ),
-                                );
-                              }
-                            }
-                          : null,
-                      child: Container(
-                        height: height,
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          content == null || content == ''
-                              ? () {
-                                  if (state is DataDictLoading)
-                                    return '$title加载中';
-                                  else if (state is DataDictError)
-                                    return '$title加载失败';
-                                  else if (state is DataDictInitial)
-                                    return '$tipText';
-                                  else if (state is DataDictLoaded)
-                                    return '请选择$title';
-                                  else
-                                    return '未知状态，state=$state';
-                                }()
-                              : content,
-                          textAlign: TextAlign.right,
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: content == null || content == ''
-                                ? () {
-                                    if (state is DataDictError)
-                                      return Colors.red;
-                                    else
-                                      return Colours.secondary_text;
-                                  }()
-                                : Colours.primary_text,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                        ),
-                      ),
-                    ),
-                    onSelected: onSelected,
-                    itemBuilder: (BuildContext context) {
-                      if (state is DataDictLoaded) {
-                        return state.dataDictList
-                            .map<PopupMenuItem<DataDict>>((dataDict) {
-                          return CheckedPopupMenuItem<DataDict>(
-                            value: dataDict,
-                            checked: selected?.contains(dataDict) ?? false,
                             child: Text('${dataDict.name}'),
                           );
                         }).toList();
@@ -512,6 +346,7 @@ class DataDictBlocGrid extends StatelessWidget {
 /// [DataDictDialog]点击修改按钮的回调函数
 typedef ConfirmCallBack = void Function(List<DataDict> dataDictList);
 
+/// 数据字典多选Dialog
 class DataDictDialog extends StatefulWidget {
   final String title;
   final String imagePath;
@@ -545,9 +380,12 @@ class _DataDictDialogState extends State<DataDictDialog> {
     return Material(
       type: MaterialType.transparency,
       child: Center(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width / 1.3,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height / 1.3,
+          ),
           child: Container(
+            width: MediaQuery.of(context).size.width / 1.3,
             padding: const EdgeInsets.all(16),
             decoration: ShapeDecoration(
               color: Color(0xFFFFFFFF),
@@ -576,46 +414,47 @@ class _DataDictDialogState extends State<DataDictDialog> {
                   ],
                 ),
                 Gaps.vGap16,
-                GridView.count(
-                  crossAxisCount: 2,
-                  childAspectRatio: 2.5,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  children: widget.dataDictList.map<Widget>((dataDict) {
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          if (checkList.contains(dataDict))
-                            checkList.remove(dataDict);
-                          else {
-                            checkList.add(dataDict);
-                          }
-                        });
-                      },
-                      child: Container(
-                        height: 40,
-                        decoration: BoxDecoration(
-                          color: checkList.contains(dataDict)
-                              ? Colors.lightBlueAccent
-                              : Colours.divider_color,
-                          borderRadius: BorderRadius.circular(2),
-                        ),
-                        child: Center(
-                          child: Text(
-                            '${dataDict.name}',
-                            style: TextStyle(
-                              fontSize: 14,
-                              color: checkList.contains(dataDict)
-                                  ? Colors.white
-                                  : Colours.secondary_text,
+                Flexible(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    childAspectRatio: 2.5,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    shrinkWrap: true,
+                    children: widget.dataDictList.map<Widget>((dataDict) {
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            if (checkList.contains(dataDict))
+                              checkList.remove(dataDict);
+                            else {
+                              checkList.add(dataDict);
+                            }
+                          });
+                        },
+                        child: Container(
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: checkList.contains(dataDict)
+                                ? Colors.lightBlueAccent
+                                : Colours.divider_color,
+                            borderRadius: BorderRadius.circular(2),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '${dataDict.name}',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: checkList.contains(dataDict)
+                                    ? Colors.white
+                                    : Colours.secondary_text,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    );
-                  }).toList(),
+                      );
+                    }).toList(),
+                  ),
                 ),
                 Gaps.vGap16,
                 Row(
@@ -720,9 +559,12 @@ class _DataDictBlocDialogState extends State<DataDictBlocDialog> {
     return Material(
       type: MaterialType.transparency,
       child: Center(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width / 1.3,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height / 1.3,
+          ),
           child: Container(
+            width: MediaQuery.of(context).size.width / 1.3,
             padding: const EdgeInsets.all(16),
             decoration: ShapeDecoration(
               color: Color(0xFFFFFFFF),
@@ -766,46 +608,48 @@ class _DataDictBlocDialogState extends State<DataDictBlocDialog> {
                             ],
                           ),
                           Gaps.vGap16,
-                          GridView.count(
-                            crossAxisCount: 2,
-                            childAspectRatio: 2.5,
-                            crossAxisSpacing: 10,
-                            mainAxisSpacing: 10,
-                            shrinkWrap: true,
-                            physics: NeverScrollableScrollPhysics(),
-                            children: state.dataDictList.map<Widget>((dataDict) {
-                              return GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    if (checkList.contains(dataDict))
-                                      checkList.remove(dataDict);
-                                    else {
-                                      checkList.add(dataDict);
-                                    }
-                                  });
-                                },
-                                child: Container(
-                                  height: 40,
-                                  decoration: BoxDecoration(
-                                    color: checkList.contains(dataDict)
-                                        ? Colors.lightBlueAccent
-                                        : Colours.divider_color,
-                                    borderRadius: BorderRadius.circular(2),
-                                  ),
-                                  child: Center(
-                                    child: Text(
-                                      '${dataDict.name}',
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: checkList.contains(dataDict)
-                                            ? Colors.white
-                                            : Colours.secondary_text,
+                          Flexible(
+                            child: GridView.count(
+                              crossAxisCount: 2,
+                              childAspectRatio: 2.5,
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10,
+                              shrinkWrap: true,
+                              children:
+                                  state.dataDictList.map<Widget>((dataDict) {
+                                return GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      if (checkList.contains(dataDict))
+                                        checkList.remove(dataDict);
+                                      else {
+                                        checkList.add(dataDict);
+                                      }
+                                    });
+                                  },
+                                  child: Container(
+                                    height: 40,
+                                    decoration: BoxDecoration(
+                                      color: checkList.contains(dataDict)
+                                          ? Colors.lightBlueAccent
+                                          : Colours.divider_color,
+                                      borderRadius: BorderRadius.circular(2),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        '${dataDict.name}',
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          color: checkList.contains(dataDict)
+                                              ? Colors.white
+                                              : Colours.secondary_text,
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                              );
-                            }).toList(),
+                                );
+                              }).toList(),
+                            ),
                           ),
                           Gaps.vGap16,
                           Row(
@@ -870,7 +714,8 @@ class _DataDictBlocDialogState extends State<DataDictBlocDialog> {
                     } else {
                       return RowErrorWidget(
                         errorMessage: 'BlocBuilder监听到未知的的状态!state=$state',
-                        onReloadTap: () => widget.dataDictBloc.add(DataDictLoad()),
+                        onReloadTap: () =>
+                            widget.dataDictBloc.add(DataDictLoad()),
                       );
                     }
                   },
