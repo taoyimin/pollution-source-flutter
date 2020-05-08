95a85a40d57f52efe44052395722e921810cba1e
taoyimin <1103995288@qq.com>
2020-05-08 17:19:34 +0800

    setup jpush

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,22 @@ import 'package:fluro/fluro.dart';
 import 'package:flustars/flustars.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:jpush_flutter/jpush_flutter.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:pollution_source/module/login/login_page.dart';
+import 'package:pollution_source/page/admin_home.dart';
+import 'package:pollution_source/page/enter_home.dart';
+import 'package:pollution_source/page/operation_home.dart';
 import 'package:pollution_source/res/colors.dart';
+import 'package:pollution_source/res/constant.dart';
 import 'package:pollution_source/route/application.dart';
 import 'package:pollution_source/route/routes.dart';
+
+import 'module/common/list/list_bloc.dart';
+import 'module/warn/list/warn_list_page.dart';
+import 'module/warn/list/warn_list_repository.dart';
 
 /// 应用程序入口
 ///
@@ -15,8 +25,8 @@ import 'package:pollution_source/route/routes.dart';
 /// 全局状态栏使用白色字体
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await SpUtil.getInstance();
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+  await SpUtil.getInstance();
   runApp(MyApp());
 }
 
@@ -32,10 +42,37 @@ class MyApp extends StatefulWidget {
 /// [build]方法最外层包裹一层[OKToast]用于弹出吐司
 /// 并且在[MaterialApp]中设置程序的中文本地化，主题样式和路由
 class _MyAppState extends State<MyApp> {
+  final GlobalKey<NavigatorState> navigatorKey =
+      new GlobalKey<NavigatorState>();
+
   _MyAppState() {
+    // 初始化路由
     final router = Router();
     Routes.configureRoutes(router);
     Application.router = router;
+    // 初始化JPush
+    JPush jpush = JPush();
+    jpush.addEventHandler(
+      // 接收通知回调方法。
+      onReceiveNotification: (Map<String, dynamic> message) async {},
+      // 点击通知回调方法。
+      onOpenNotification: (Map<String, dynamic> message) async {
+        navigatorKey.currentState.push(MaterialPageRoute(
+            builder: (context) => BlocProvider<ListBloc>(
+                  create: (BuildContext context) =>
+                      ListBloc(listRepository: WarnListRepository()),
+                  child: WarnListPage(),
+                )));
+      },
+      // 接收自定义消息回调方法。
+      onReceiveMessage: (Map<String, dynamic> message) async {},
+    );
+    jpush.setup(
+      appKey: 'eb5a21dcceead742e52f47a5',
+      production: Constant.inProduction,
+      debug: !Constant.inProduction, // 设置是否打印 debug 日志
+    );
+    jpush.applyPushAuthority();
   }
 
   @override
@@ -48,6 +85,7 @@ class _MyAppState extends State<MyApp> {
           FocusScope.of(context).requestFocus(FocusNode());
         },
         child: MaterialApp(
+          navigatorKey: navigatorKey,
           localizationsDelegates: [
             GlobalMaterialLocalizations.delegate,
             GlobalWidgetsLocalizations.delegate,
@@ -67,8 +105,31 @@ class _MyAppState extends State<MyApp> {
               //subtitle1: TextStyle(textBaseline: TextBaseline.alphabetic),
             ),
           ),
-          //onGenerateRoute: Application.router.generator,
-          home: LoginPage(),
+//          onGenerateRoute: Application.router.generator,
+          home: () {
+            int userType = SpUtil.getInt(Constant.spUserType, defValue: -1);
+            int userId = SpUtil.getInt(Constant.spUserId, defValue: -1);
+            if (userType != -1 &&
+                userId != -1 &&
+                !TextUtil.isEmpty(
+                    SpUtil.getString(Constant.spUsernameList[userType])) &&
+                !TextUtil.isEmpty(
+                    SpUtil.getString(Constant.spPasswordList[userType]))) {
+              // 如果userType、userId、userName、passWord都不为空，则跳过登录直接进入主页
+              switch (userType) {
+                case 0:
+                  return AdminHomePage();
+                case 1:
+                  return EnterHomePage(enterId: '$userId');
+                case 2:
+                  return OperationHomePage();
+                default:
+                  return LoginPage();
+              }
+            } else {
+              return LoginPage();
+            }
+          }(),
         ),
       ),
     );
